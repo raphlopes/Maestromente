ef1482b4ed94f09ba170671c48f2fd4e5633d4ee
Raphael Mac <lopesraphael94@gmail.com>
2022-03-12 18:37:17 +0100

    COmmit react et firebase

--- maestromente/src/App.js
+++ maestromente/src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bite <code>src/App.js</code> and save to reload.
+          <h1> Test html </h1>
         </p>
         <a
           className="App-link"
