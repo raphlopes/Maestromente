766f1e608848baad4a23a477ac1fed11fc359be7
Nqueral <nicolas06041997@gmail.com>
2022-03-13 15:34:37 +0100

    J'ai installé bootstrap et viré le logo react

--- maestromente/src/App.js
+++ maestromente/src/App.js
@@ -1,26 +1,16 @@
 import logo from './logo.svg';
+import './bootstrap/css/bootstrap-grid.min.css';
 import './App.css';
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Bonjour !</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Maestromende</h1>
+        <div class="container-fluid red">
+          <p>Placez vos pions ici !</p>
+        </div>
       </header>
     </div>
   );
 }
-
 export default App;
