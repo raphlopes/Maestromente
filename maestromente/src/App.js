fa59851a465635b4d961faa895d4568dbd246cca
Nqueral <nicolas06041997@gmail.com>
2022-03-13 21:04:21 +0100

    Tried to link a js file named "plateau" to the App.js file but doesn't work sniffff

--- maestromente/src/App.js
+++ maestromente/src/App.js
@@ -1,15 +1,15 @@
 import logo from './logo.svg';
 import './bootstrap/css/bootstrap-grid.min.css';
 import './App.css';
+import Plateau from './Plateau';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Maestromende</h1>
-        <div class="container-fluid red">
           <p>Placez vos pions ici !</p>
-        </div>
       </header>
+      <Plateau />
     </div>
   );
 }
---
+++ maestromente/src/Plateau.js
@@ -0,0 +1,14 @@
+
+const Plateau =() => {
+    return(
+        <div className="Plateau">
+            <div class="d-flex p-3 bg-secondary text-white">
+                <div class="p-2 bg-info">Flex item 1</div>
+                <div class="p-2 bg-warning">Flex item 2</div>
+                <div class="p-2 bg-primary">Flex item 3</div>
+            </div>
+            </div>  
+    );
+};
+
+export default Plateau;
